9a6788096294e4ae5f6ad7be36d07a797facda3e
alena <pylkaya@ya.ru>
2021-11-08 16:07:39 +0200

    feat: 05 task is completed

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,6 +1,16 @@
-const { readdir, readFile, stat } = require('fs/promises');
-const { join } = require('path');
+const { readdir, readFile, writeFile, appendFile } = require('fs/promises');
+const { join, extname } = require('path');
 
 const stylesPath = join(__dirname, 'styles');
-const bandlePath = join(__dirname, 'files-copy', 'project-dist', 'bundle.css');
+const bundlePath = join(__dirname, 'project-dist', 'bundle.css');
 
+async function createStyleFile() {
+    await writeFile(bundlePath, '');
+    const items = await readdir(stylesPath, { withFileTypes: true });
+    for (let item of items) {
+        const itemContent = await readFile(join(stylesPath, item.name))
+            if (item.isFile() == true && extname(item.name).slice(1) == 'css');
+            await appendFile(bundlePath, itemContent);
+        }
+}
+createStyleFile();
