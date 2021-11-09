598fc2b489e121c511128ee05f35a3568cef77b7
alena <pylkaya@ya.ru>
2021-11-09 10:24:57 +0200

    fix: delete ; sign in 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -9,7 +9,7 @@ async function createStyleFile() {
     const items = await readdir(stylesPath, { withFileTypes: true });
     for (let item of items) {
         const itemContent = await readFile(join(stylesPath, item.name))
-            if (item.isFile() == true && extname(item.name).slice(1) == 'css');
+            if (item.isFile() == true && extname(item.name).slice(1) == 'css')
             await appendFile(bundlePath, itemContent);
         }
 }
